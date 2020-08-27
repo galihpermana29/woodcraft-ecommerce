a67d86306a7b668bc42087a6a97dc6a5834c8594
galihpermana29 <galihcbn123@gmail.com>
2020-08-28 02:54:29 +0700

    notification card cartNumber fixed

--- script.js
+++ script.js
@@ -1,46 +1,80 @@
-const cardButton = document.querySelectorAll('.card-button')
-Array.from(cardButton)
+const cardButton = Array.from(document.querySelectorAll('.card-button'));
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('#overlay');
+const cards = document.querySelector('.cards');
+const cross = document.querySelector('.fa-times');
 
-const name = ['Original Phone Holder', 'Horse Phone Holder', 'Deer Phone Holder']
-const price = ['$15', '$10', '$15']
-
-for(let i = 0; i < cardButton.length; i++) {
-   cardButton[i].addEventListener('click', () => {
-      const modal = document.querySelector('.modal')
-      const overlay = document.querySelector('#overlay')
-      modal.classList.add('active')
-      overlay.classList.add('active')
-
-      const modalImg = document.querySelector('.modal-image')
-      modalImg.setAttribute(`src`, `image/${i+1}.png`)
-
-      const priceEl = document.querySelector('.title-prod h3')
-      const nameEl = document.querySelector('.title-prod h2')
-      priceEl.innerHTML = `${price[i]}`;
-      nameEl.innerHTML = `${name[i]}`;
-
-      const addToCart = document.querySelectorAll('.cart')
-      for(let j = 0; j < addToCart.length; j++) {
-         addToCart[j].addEventListener('click', ()=> {
-            const notification = document.querySelector('.notification')
-            notification.classList.add('active')
-            modal.classList.remove('active')
-            overlay.classList.remove('active')
-
-            const nameBrand = document.querySelector('p.name-brand')
-            const message = document.querySelector('p.message')
-            nameBrand.innerHTML = `${name[i]}`;
-            message.innerHTML = `Successfully added to cart`
-            setTimeout(() => {
-               notification.classList.remove('active')
-            },2000)
-         })
-      }
-
-      const cross = document.querySelector('.fa-times')
-      cross.addEventListener('click', () => {
-         modal.classList.remove('active')
-         overlay.classList.remove('active')
-      })
-   })
+function stickyNav() {
+	const nav = document.querySelector('nav');
+	if (window.scrollY > 0) {
+		nav.classList.add('sticky');
+	} else {
+		nav.classList.remove('sticky');
+	}
 }
+
+function closeModal() {
+	modal.classList.remove('active');
+	overlay.classList.remove('active');
+}
+
+function showModal(e) {
+	modal.classList.add('active');
+	overlay.classList.add('active');
+	let src = e.target.previousElementSibling.previousElementSibling.previousElementSibling.getAttribute(
+		'src'
+	);
+	let price = e.target.previousElementSibling.textContent;
+	let name =
+		e.target.previousElementSibling.previousElementSibling.textContent;
+	const modalImg = document.querySelector('.modal-image');
+	modalImg.setAttribute(`src`, `${src}`);
+
+	const priceEl = document.querySelector('.title-prod h3');
+	const nameEl = document.querySelector('.title-prod h2');
+	priceEl.innerHTML = `${price}`;
+	nameEl.innerHTML = `${name}`;
+}
+
+let sum = 0;
+
+function showNotification(e) {
+	const notification = document.querySelector('.notification');
+	notification.classList.add('active');
+	modal.classList.remove('active');
+	overlay.classList.remove('active');
+	let name =
+		e.target.parentElement.previousElementSibling.previousElementSibling
+			.children[1].textContent;
+
+	const nameBrand = document.querySelector('p.name-brand');
+	const message = document.querySelector('p.message');
+	nameBrand.innerHTML = `${name}`;
+	message.innerHTML = `Successfully added to cart`;
+	setTimeout(() => {
+		notification.classList.remove('active');
+	}, 2000);
+
+	sum += 1;
+
+   let cartNum = document.querySelector('.circle');
+   cartNum.classList.add('num')
+   setTimeout(()=> {
+      cartNum.classList.remove('num')
+   },1000)
+	cartNum.innerHTML = `${sum}`;
+}
+
+modal.addEventListener('click', (e) => {
+	if (e.target.className == 'cart') showNotification(e);
+});
+
+cross.addEventListener('click', closeModal);
+
+window.addEventListener('scroll', stickyNav);
+
+cards.addEventListener('click', (e) => {
+	if (e.target.className == 'card-button det1') showModal(e);
+	if (e.target.className == 'card-button det2') showModal(e);
+	if (e.target.className == 'card-button det3') showModal(e);
+});
