2540c82e8efbd417ea7bb8a2b9ac487ffd081607
galihpermana29 <galihcbn123@gmail.com>
2020-09-01 02:41:37 +0700

    fix without checkout and delete button

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const cardButton = Array.from(document.querySelectorAll('.card-button'));
 const modal = document.querySelector('.modal');
+const modalImgEl = document.querySelector('.modal-header img');
 const overlay = document.querySelector('#overlay');
 const cards = document.querySelector('.cards');
 const cross = document.querySelector('.fa-times');
+const cartPage = document.querySelector('.cart-page');
+const cartIcon = document.querySelector('.fa-shopping-cart');
+const chevronDown = document.querySelector('.fa-chevron-down');
+
+let sum = 0;
+let cartList = [];
 
 function stickyNav() {
 	const nav = document.querySelector('nav');
@@ -16,6 +23,14 @@ function stickyNav() {
 function closeModal() {
 	modal.classList.remove('active');
 	overlay.classList.remove('active');
+	setTimeout(() => {
+		if (modalImgEl.classList.value == 'card-img1')
+			modalImgEl.classList.remove('card-img1');
+		if (modalImgEl.classList.value == 'card-img2')
+			modalImgEl.classList.remove('card-img2');
+		if (modalImgEl.classList.value == 'card-img3')
+			modalImgEl.classList.remove('card-img3');
+	}, 500);
 }
 
 function showModal(e) {
@@ -27,46 +42,127 @@ function showModal(e) {
 	let price = e.target.previousElementSibling.textContent;
 	let name =
 		e.target.previousElementSibling.previousElementSibling.textContent;
-	const modalImg = document.querySelector('.modal-image');
-	modalImg.setAttribute(`src`, `${src}`);
+	if (src === 'image/1.png') modalImgEl.classList.add('card-img1');
+	if (src === 'image/2.png') modalImgEl.classList.add('card-img2');
+	if (src === 'image/3.png') modalImgEl.classList.add('card-img3');
+	modalImgEl.setAttribute(`src`, `${src}`);
 
 	const priceEl = document.querySelector('.title-prod h3');
 	const nameEl = document.querySelector('.title-prod h2');
 	priceEl.innerHTML = `${price}`;
 	nameEl.innerHTML = `${name}`;
+	overlay.addEventListener('click', closeModal);
 }
 
-let sum = 0;
-
-function showNotification(e) {
+function showNotification(name) {
 	const notification = document.querySelector('.notification');
 	notification.classList.add('active');
-	modal.classList.remove('active');
-	overlay.classList.remove('active');
+	closeModal();
+
+	const nameBrand = document.querySelector('p.name-brand');
+	nameBrand.innerHTML = `${name}`;
+	message();
+	setTimeout(() => {
+		notification.classList.remove('active');
+	}, 2500);
+}
+
+function message(message) {
+	const messageEl = document.querySelector('p.message');
+	messageEl.innerHTML = `${message}`;
+}
+
+function addToCart(e) {
 	let name =
 		e.target.parentElement.previousElementSibling.previousElementSibling
 			.children[1].textContent;
+	let price =
+		e.target.parentElement.previousElementSibling.previousElementSibling
+			.children[0].textContent;
+	let imageSrc = e.target.parentElement.previousElementSibling.previousElementSibling.parentElement.previousElementSibling.children[1].getAttribute(
+		'src'
+	);
+	let quantity = e.target.previousElementSibling.value;
 
-	const nameBrand = document.querySelector('p.name-brand');
-	const message = document.querySelector('p.message');
-	nameBrand.innerHTML = `${name}`;
-	message.innerHTML = `Successfully added to cart`;
-	setTimeout(() => {
-		notification.classList.remove('active');
-	}, 2000);
+	const cartPageBody = document.querySelector('.cart-page-body');
+	if (cartList.includes(name)) {
+		showNotification(name);
+		message('Already in your cart');
+	} else {
+		cartList.push(name);
+		cartPageBody.innerHTML += `
+         <div class="cart-1">
+            <div class="check">
+               <input type="checkbox" name="cart-check" id="cart-check" checked >
+            </div>
+            <div class="item">
+               <img src="${imageSrc}" alt="" width="70px">
+            </div>
+            <div class="item-detail">
+               <p>${name}</p>
+               <p>${price}</p>
+               <input type="number" name="quantity" id="quantity" autofocus value="${quantity}" min="1" max="10">
+            </div>
+         </div> `;
+		cartCount();
+		showNotification(name);
+		message('Successfully added to you cart');
+		showAddtoCartDeleteButton();
+	}
+}
 
+function cartCount() {
 	sum += 1;
+	let cartNum = document.querySelector('.circle');
+	cartNum.classList.add('num');
 
-   let cartNum = document.querySelector('.circle');
-   cartNum.classList.add('num')
-   setTimeout(()=> {
-      cartNum.classList.remove('num')
-   },1000)
+	setTimeout(() => {
+		cartNum.classList.remove('num');
+	}, 1000);
 	cartNum.innerHTML = `${sum}`;
 }
 
+function showAddtoCartDeleteButton() {
+	const checkoutButtonEl = document.querySelector('.checkout-button');
+	const deleteButtonEl = document.querySelector('.delete-button');
+	const cartStatusEl = document.querySelector('.cart-status');
+
+	cartStatusEl.classList.add('active');
+	checkoutButtonEl.classList.add('active');
+	deleteButtonEl.classList.add('active');
+}
+
+function closeCartPage() {
+	cartPage.classList.remove('active');
+	overlay.classList.remove('active');
+}
+
+function showCartPage() {
+	cartPage.classList.add('active');
+	overlay.classList.add('active');
+}
+
+function testiCarousel() {
+	const testimonials = document.querySelectorAll('.testi');
+	let t = 0;
+	setInterval(() => {
+		if (t === 4) t = 0;
+		testimonials[t].classList.add('active');
+		setTimeout(() => {
+			testimonials[t].classList.remove('active');
+			t += 1;
+		}, 5900);
+	}, 3000);
+}
+
+testiCarousel();
+cartIcon.addEventListener('click', showCartPage);
+
+overlay.addEventListener('click', closeCartPage);
+chevronDown.addEventListener('click', closeCartPage);
+
 modal.addEventListener('click', (e) => {
-	if (e.target.className == 'cart') showNotification(e);
+	if (e.target.className == 'cart') addToCart(e);
 });
 
 cross.addEventListener('click', closeModal);
